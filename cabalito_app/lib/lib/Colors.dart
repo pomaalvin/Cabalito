35bcb38052d1f2bfc74d29d1b303a45eaa28b539
pjalvin <pomaalvin@gmail.com>
2020-11-20 01:03:20 -0400

    mejorando template de mecanicos

---
+++ cabalito_app/lib/lib/Colors.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+var color1=Color.fromRGBO(20, 61, 89, 1);
+var color2=Color.fromRGBO(244, 180, 26, 1);
+var color3=Colors.black;
+var color4=Colors.black;
--- cabalito_app/lib/screens/MechanicList.dart
+++ cabalito_app/lib/screens/MechanicList.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import '../lib/Colors.dart';
 
 class MechanicList extends StatefulWidget{
   @override
@@ -11,84 +12,100 @@ class MechanicListState extends State<MechanicList>{
   void initState(){
     super.initState();
   }
+  Size size;
   @override
   Widget build(BuildContext context) {
+    size=MediaQuery.of(context).size;
     return new Scaffold(
-      backgroundColor: Color.fromRGBO(20, 61, 89, 1),
-      body: ListView(
+      backgroundColor: color1,
+      body: Column(
         children: <Widget>[
-          Padding(
-            padding:EdgeInsets.only(top: 15.0, left:10.0),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: <Widget>[
-                IconButton(
-                    icon: Icon(Icons.menu),
-                    color: Colors.white,
-                    iconSize: 28.0,
-                    onPressed: (){
-                    }
-                ),
+          Container(
+            height:110,
+            child: Center(
+
+              child:
                 Container(
-                  width: MediaQuery.of(context).size.width-100.0,
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                      Text("Mecánicos",
+                  padding: EdgeInsets.only(top:20),
+                      child:Text("Mecánicos",
                         style: TextStyle(
                           color: Colors.white,
                           fontWeight: FontWeight.bold,
                           fontSize: 25.0
                         ),
                       ),
-                    ],
-                  ),
                 ),
-              ],
             ),
           ),
-          SizedBox(
-            height: 25.0,
-          ),
-          Stack(
-            children: <Widget>[
-              Container(
-                height: MediaQuery.of(context).size.height,
-                decoration: BoxDecoration(
-                  color: Color.fromRGBO(244, 180, 26, 1),
-                  borderRadius: BorderRadius.only(topLeft: Radius.circular(75.0)),
-                ),
-              ),
-              Container(
-                margin: new EdgeInsets.only(top: 6.0),
-                height: MediaQuery.of(context).size.height,
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.only(topLeft: Radius.circular(75.0)),
-                ),
-                child: ScrollConfiguration(
-                  behavior: MyBehavior(),
-                  child:ListView(
-                      primary: false,
-                      padding: EdgeInsets.only(left:25.0, right: 20.0),
-                      children: <Widget>[
-                        Padding(
-                          padding: EdgeInsets.only(top: 25.0),
-                          child: Container(
-                            height: MediaQuery.of(context).size.height-150.0,
-                            child: ListView.builder(
-                              itemBuilder: (context, index){
-                                return ListTile(title: Text('Item $index'),);
-                              },
-                              itemCount: 100,
-                            ),
+          Expanded(
+            child: Stack(
+              children: <Widget>[
+                  Container(
+                    decoration: BoxDecoration(
+                      color: color2,
+                      borderRadius: BorderRadius.only(topLeft: Radius.circular(75.0)),
+                    ),
+                  ),
+                  Container(
+                    margin: new EdgeInsets.only(top: 6.0),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.only(topLeft: Radius.circular(75.0)),
+                    ),
+                    child: Column(
+                      children: [
+                        Container(
+                          height: 100,
+                          padding: EdgeInsets.only(left:size.width*0.1,right:size.width*0.1),
+                          child: Center(
+                            child: Busqueda()
                           ),
                         ),
+                        Expanded(
+                          child: ScrollConfiguration(
+                            behavior: MyBehavior(),
+                            child: GridView.builder(
+                              padding: EdgeInsets.all(0),
+                              gridDelegate:SliverGridDelegateWithFixedCrossAxisCount(
+                                crossAxisCount: 2,
+                                crossAxisSpacing: 2,
+                                mainAxisSpacing: 2
+                              ),
+                              itemBuilder: (context, index){
+                                return Container(
+                                  color: Colors.black.withOpacity(0.5),
+                                  child: Column(
+                                    children: [
+                                      Container(
+                                          width: size.width*0.5,
+                                          height: size.width*0.5*0.8,
+                                        decoration: BoxDecoration(
+                                          image: DecorationImage(
+                                            fit: BoxFit.fill,
+                                            image: NetworkImage(
+                                              "https://lh3.googleusercontent.com/proxy/w03jEsMOLft4L9aoN-sXooFysioVPF9EorLo8RwCNupJvhMpX0OjjZ_Mh3VJHVh3ttAqN9tVlmPp0GbB9N0DZgm01g"
+                                            )
+                                          )
+                                        )
+                                      ),
+                                      Expanded(
+                                        child: Center(
+                                          child: Estrellas(((index/5)+1%5).floor()),
+                                        )
+                                      ),
+                                    ],
+                                  )
+                                );
+                              },
+                              itemCount: 20,
+                            ),
+                          ),
+                        )
                       ],
-                    ),
-                ),
-              )
-            ],
+                    )
+                  ),
+              ],
+            ),
           ),
         ],
       )
@@ -102,4 +119,58 @@ class MyBehavior extends ScrollBehavior {
       BuildContext context, Widget child, AxisDirection axisDirection) {
     return child;
   }
+}
+class Estrellas extends StatelessWidget{
+  int estrellas;
+  Estrellas(this.estrellas);
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return  Container(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: List.generate(5, (index) {
+          if(estrellas>=index+1){
+
+            return
+              Icon(Icons.star,color: color2,);
+          }
+          else{
+
+            return
+              Icon(Icons.star_border,color: color2,);
+          }
+        })
+      ),
+    );
+  }
+
+}
+class Busqueda extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+   return Container(
+     decoration: BoxDecoration(
+       borderRadius: BorderRadius.circular(10),
+       border: Border.all(color: color3.withOpacity(0.4),width: 2)
+     ),
+     child: TextField(
+       cursorColor: color3.withOpacity(0.6),
+       cursorWidth: 1,
+       style: TextStyle(color: color3.withOpacity(0.6),fontSize: 18),
+       decoration: InputDecoration(
+         fillColor: Colors.white,
+         border: InputBorder.none,
+         contentPadding: EdgeInsets.only(left: 10,top: 15,bottom: 0,right: 0),
+         hintText: "Buscar mecánicos",
+         suffixIcon:Icon(Icons.search,color: color3.withOpacity(0.6),) ,
+
+         hintStyle: TextStyle(color: color3.withOpacity(0.3)),
+         // focusColor: Colors.white,
+         // labelStyle: TextStyle(color: Colors.white),
+       ),
+     ),
+   );
+  }
+
 }
